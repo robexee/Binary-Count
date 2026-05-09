04bb7a65ecd6f284d4903c1f56a1518b96cfc2da
robexee <davidrobertmihai@gmail.com>
2026-05-09 13:57:11 +0300

    Restructure the project

--- camera.py
+++ camera.py
@@ -2,61 +2,83 @@ import cv2 as cv
 import mediapipe as mp
 
 
-mp_drawing = mp.solutions.drawing_utils
-mp_hands = mp.solutions.hands
-hands = mp_hands.Hands(min_detection_confidence=0.5, min_tracking_confidence=0.5, max_num_hands = 1)
-cirlce_color = mp_drawing.DrawingSpec(color = (0,255,0), thickness = 3, circle_radius = 1)
+class HandBinaryTracker:
+
+    def __init__(self, max_hands: int = 1, detection_conf: float = 0.5, tracking_conf:  float = 0.5):
+        self.mp_drawing = mp.solutions.drawing_utils
+        self.mp_hands = mp.solutions.hands
+        self.hands = self.mp_hands.Hands(
+            min_detection_confidence = detection_conf,
+            min_tracking_confidence = tracking_conf,
+            max_num_hands = max_hands
+        )
+        self.circle_color = self.mp_drawing.DrawingSpec(color=(0,255,0), thickness = 3, circle_radius = 1)
+
+    def get_binary_count(self, hand_landmarks, hand_label):
+        lm = hand_landmarks.landmark
+
+        if hand_label == 'Right':
+            thumb = lm[4].x < lm[3].x
+        else:
+            thumb = lm[4].x > lm[3].x
+
+        fingers = [
+            thumb,
+            lm[8].y < lm[6].y,
+            lm[12].y < lm[10].y,
+            lm[16].y < lm[14].y,
+            lm[20].y < lm[18].y
+        ]
+
+        decimal_value = sum(state << i for i, state in enumerate(fingers))
+        binary_string = "".join(str(int(state)) for state in fingers[::-1])
+
+        return decimal_value, binary_string
+    
+    def draw(self, frame, hand_landmarks):
+        self.mp_drawing.draw_landmarks(
+            frame, hand_landmarks, self.mp_hands.HAND_CONNECTIONS, self.circle_color
+        )
 
 
-def resize_camera(frame, scale = 0.75):
+def resize_camera(frame, scale: float = 0.75):
     width = int(frame.shape[1] * scale)
     height = int(frame.shape[0] * scale)
-    dimensions = (width, height)
-    resized_frame = cv.resize(frame, dimensions, interpolation=cv.INTER_AREA)
-    return resized_frame
-
-
-def binary_count(hand_landmarks, hand_label):
-    lm = hand_landmarks.landmark
-
-    if hand_label == 'Right':
-        thumb = lm[4].x < lm[3].x
-    else:
-        thumb = lm[4].x > lm[3].x
+    return cv.resize(frame, (width, height), interpolation=cv.INTER_AREA)
     
-    fingers = [
-        thumb,
-        lm[8].y < lm[6].y,
-        lm[12].y < lm[10].y,
-        lm[16].y < lm[14].y,
-        lm[20].y < lm[18].y
-    ]
-    decimal_value = sum(state << i for i, state in enumerate(fingers))
-    binary_value = "".join(str(int(state)) for state in fingers[::-1])
-    return decimal_value, binary_value
 
+def main():
+    video = cv.VideoCapture(0)
+    tracker = HandBinaryTracker()
 
-video = cv.VideoCapture(0)
-while True:
-    isTrue, frame = video.read()
-    frame = cv.flip(frame, 1)
-    frame = resize_camera(frame, 2)
-    rgb_frame = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
-    results = hands.process(rgb_frame)
+    while True:
+        isTrue, frame = video.read()
+        if not isTrue:
+            break
+
+        frame = cv.flip(frame, 1)
+        frame = resize_camera(frame, 2)
+        rgb_frame = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
+
+        results = tracker.hands.process(rgb_frame)
+
+        if results.multi_hand_landmarks:
+            for hand_landmarks, handedness in zip (results.multi_hand_landmarks, results.multi_handedness):
+                hand_label = handedness.classification[0].label
+
+                decimal_val, binary_str = tracker.get_binary_count(hand_landmarks, hand_label)
+                tracker.draw(frame, hand_landmarks)
+
+                cv.putText(frame, f"Decimal: {decimal_val}", (10,40), cv.FONT_HERSHEY_COMPLEX, 1, (255,255,255), 1)
+                cv.putText(frame, f"Binary: {binary_str}", (frame.shape[1] - 250, 40), cv.FONT_HERSHEY_COMPLEX, 1, (255,255,255), 1)
+        
+        cv.imshow('Binary Finger Counter', frame)
+
+        if cv.waitKey(10) & 0xFF == ord('q'):
+            break
     
-    if results.multi_hand_landmarks:
-        for hand_landmarks, handedness in zip(results.multi_hand_landmarks, results.multi_handedness):
-            hand_label = handedness.classification[0].label
-            
-            decimal_val, binary_str = binary_count(hand_landmarks, hand_label)
-            mp_drawing.draw_landmarks(frame, hand_landmarks, mp_hands.HAND_CONNECTIONS, cirlce_color)
+    video.release()
+    cv.destroyAllWindows()
 
-            cv.putText(frame, f"Decimal Number: {decimal_val}", (10, 40), cv.FONT_HERSHEY_COMPLEX, 1, (255,255,255), 1)
-            cv.putText(frame, f"Binary: {binary_str}", (frame.shape[1] - 250, 46), cv.FONT_HERSHEY_COMPLEX,1 , (255,255,255), 1)
-
-    cv.imshow('Binary Count', frame)
-    if cv.waitKey(10) & 0xFF==ord('q'):
-        break
-
-video.release()
-cv.destroyAllWindows()
+if __name__ == '__main__':
+    main()
